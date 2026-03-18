0e832501a37914da24b197712ec634e5544c6df5
vitormoraess <vitor.nascimento.moraes92@gmail.com>
2026-03-18 20:58:26 -0300

    Criado a controller

---
+++ src/main/java/com/java10x/CadastroApi/Controller.java
@@ -0,0 +1,19 @@
+package com.java10x.CadastroApi;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota";
+
+
+
+    }
+
+
+
+}
