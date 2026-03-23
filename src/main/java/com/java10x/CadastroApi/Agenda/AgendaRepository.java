0668b1636f42915252d0c8a998254388283ee757
vitormoraess <vitor.nascimento.moraes92@gmail.com>
2026-03-23 20:48:13 -0300

    feat: Adiciona repositories JPA para Paciente e Agenda

---
+++ src/main/java/com/java10x/CadastroApi/Agenda/AgendaRepository.java
@@ -0,0 +1,6 @@
+package com.java10x.CadastroApi.Agenda;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgendaRepository extends JpaRepository<AgendaModel, Long> {
+}
---
+++ src/main/java/com/java10x/CadastroApi/Pacientes/PacienteService.java
@@ -0,0 +1,4 @@
+package com.java10x.CadastroApi.Pacientes;
+
+public class PacienteService {
+}
